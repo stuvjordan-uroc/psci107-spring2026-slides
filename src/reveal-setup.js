31df67edf097a48bf74bef100b8008b6f06f900b
stu <stuvjordan@gmail.com>
2026-01-22 12:15:32 -0500

    slides for day 2 done.

--- src/reveal-setup.js
+++ src/reveal-setup.js
@@ -1,10 +1,11 @@
 import Reveal from 'reveal.js';
 import Markdown from 'reveal.js/plugin/markdown/markdown.esm.js';
+import RevealMath from 'reveal.js/plugin/math/math.esm.js';
 
 // Initialize Reveal.js with default options
 // You can customize these options per presentation by passing them from the HTML
 const deck = new Reveal({
-  plugins: [Markdown],
+  plugins: [Markdown, RevealMath.KaTeX],
   // Add your preferred default config here
   hash: true,
   transition: 'slide',
